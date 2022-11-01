b274b76197018759dabe2be93f6497ee5128702a
hyunseo <abcdefghjk5@naver.com>
2022-11-01 20:24:30 +0900

    synergy

---
+++ src/test/java/jina/hyunseo/synergy/SynergyApplicationTests.java
@@ -0,0 +1,13 @@
+package jina.hyunseo.synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
