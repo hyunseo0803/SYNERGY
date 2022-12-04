8792b74bc4844d72904fd0c5231a193bd01f285d
hyunseo <abcdefghjk5@naver.com>
2022-12-04 23:31:53 +0900

    @EnableJpa 추가

--- src/main/java/jina/hyunseo/synergy/SynergyApplication.java
+++ src/main/java/jina/hyunseo/synergy/SynergyApplication.java
@@ -2,7 +2,9 @@ package jina.hyunseo.synergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+// @SpringBootApplication(scanBasePackages = {"synergy.service.MemberService"})
+@EnableJpaAuditing
 @SpringBootApplication
 public class SynergyApplication {
 
