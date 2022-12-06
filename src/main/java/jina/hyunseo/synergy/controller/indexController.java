6c8856e739c9405c2972aa9e162bde529d0ea7c6
hyunseo <abcdefghjk5@naver.com>
2022-12-06 20:58:52 +0900

    html 타임리프 사용 addAttribute 생성 - name: 사용자 이름 picture: 사용자 프로필

--- src/main/java/jina/hyunseo/synergy/controller/indexController.java
+++ src/main/java/jina/hyunseo/synergy/controller/indexController.java
@@ -1,19 +1,26 @@
 package jina.hyunseo.synergy.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jina.hyunseo.synergy.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class indexController{
-    // private final HttpSession httpSession;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
-        
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("name", user.getName());
+            model.addAttribute("picture", user.getPicture());
+        }
         return "index";
     }
 
